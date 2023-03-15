7910b743a7bbf3f57665c40297416536c1ff295f
Ahmet3Talha <112777211+Ahmet3Talha@users.noreply.github.com>
2023-03-15 21:52:41 +0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -12,5 +12,5 @@ sıralayınız. ( IN operatörünü kullanınız.)
 3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla 
 sıralayınız. ( IN operatörünü kullanınız.)
 
-    select rental_rate , replacement_cost  from film
+    select * from film
     where (rental_rate in (0.99,2.99,4.99)) and (replacement_cost in (12.99,15.99,28.99))
